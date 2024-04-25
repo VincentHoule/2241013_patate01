acd9b72b26faca77fbbcc4400d7c35d6c9b6e749
VincentHoule <2241013@carrefour.cegepvicto.ca>
2024-04-25 08:29:09 -0400

    modif 06 2024-04-25

--- src/models/sousTaches.model.js
+++ src/models/sousTaches.model.js
@@ -12,7 +12,7 @@ const SousTaches = (sousTache) => {
 SousTaches.ajouterSousTache = (tache_id, titre,  complete) => {
     return new Promise((resolve, reject) => {
 
-        const requete = 'INSERT INTO taches (tache_id, titre, complete) VALUES ($1, $2, $3) ';
+        const requete = 'INSERT INTO sous_taches (tache_id, titre, complete) VALUES ($1, $2, $3) ';
         const params = [tache_id, titre, complete]
 
         sql.query(requete, params, (erreur, resultat) => {
@@ -29,7 +29,7 @@ SousTaches.ajouterSousTache = (tache_id, titre,  complete) => {
 
 SousTaches.detailSousTache = (tache_id) => {
     return new Promise((resolve, reject) => {
-        const requete = 'SELECT * FROM sous_tache WHERE tache_id = $1';
+        const requete = 'SELECT * FROM sous_taches WHERE tache_id = $1';
         const params = [tache_id]
 
         sql.query(requete, params, (erreur, resultat) => {
@@ -45,7 +45,7 @@ SousTaches.detailSousTache = (tache_id) => {
 SousTaches.completeSousTache = (id, complete) => {
     return new Promise((resolve, reject) => {
 
-        const requete = 'UPDATE sousTaches complete = $1 WHERE id = $2 ';
+        const requete = 'UPDATE sous_taches complete = $1 WHERE id = $2 ';
         const params = [complete, parseInt(id)];
 
         sql.query(requete, params, (erreur, resultat) => {
